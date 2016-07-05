5376a4d8fcd976211388ca99f634f8003e212edb
王振 <wangzhen@hanyun.com>
2016-07-05 21:52:53 +0800

    增加对TreeMap的研究

--- wz-consumer/src/main/java/com/wz/jdk/source/MyTreeMap.java
+++ wz-consumer/src/main/java/com/wz/jdk/source/MyTreeMap.java
@@ -155,22 +155,215 @@ public class MyTreeMap<K, V> {
     }
     private void rotateLeft(Entry<K,V> p) {
         if (p != null) {
+            //获取P的右子节点，其实这里就相当于新增节点N（情况四而言）
             Entry<K,V> r = p.right;
+            //将R的左子树设置为P的右子树
             p.right = r.left;
+            //若R的左子树不为空，则将P设置为R左子树的父亲
             if (r.left != null)
                 r.left.parent = p;
+            //将P的父亲设置R的父亲
             r.parent = p.parent;
+            //如果P的父亲为空，则将R设置为跟节点
             if (p.parent == null)
                 root = r;
+            //如果P为其父节点（G）的左子树，则将R设置为P父节点(G)左子树
             else if (p.parent.left == p)
                 p.parent.left = r;
+            //否则R设置为P的父节点（G）的右子树
             else
                 p.parent.right = r;
+            //将P设置为R的左子树
             r.left = p;
+            //将R设置为P的父节点
             p.parent = r;
         }
     }
 
+    public V remove(Object key) {
+        Entry<K,V> p = getEntry(key);
+        if (p == null)
+            return null;
+
+        V oldValue = p.value;
+        deleteEntry(p);
+        return oldValue;
+    }
+    static <K,V> MyTreeMap.Entry<K,V> successor(Entry<K,V> t) {
+        if (t == null)
+            return null;
+        else if (t.right != null) {
+            Entry<K,V> p = t.right;
+            while (p.left != null)
+                p = p.left;
+            return p;
+        } else {
+            Entry<K,V> p = t.parent;
+            Entry<K,V> ch = t;
+            while (p != null && ch == p.right) {
+                ch = p;
+                p = p.parent;
+            }
+            return p;
+        }
+    }
+    private void deleteEntry(Entry<K,V> p) {
+        modCount++;
+        size--;
+
+        // If strictly internal, copy successor's element to p and then make p
+        // point to successor.
+        if (p.left != null && p.right != null) {
+            Entry<K,V> s = successor(p);
+            p.key = s.key;
+            p.value = s.value;
+            p = s;
+        } // p has 2 children
+
+        // Start fixup at replacement node, if it exists.
+        Entry<K,V> replacement = (p.left != null ? p.left : p.right);
+
+        if (replacement != null) {
+            // Link replacement to parent
+            replacement.parent = p.parent;
+            if (p.parent == null)
+                root = replacement;
+            else if (p == p.parent.left)
+                p.parent.left  = replacement;
+            else
+                p.parent.right = replacement;
+
+            // Null out links so they are OK to use by fixAfterDeletion.
+            p.left = p.right = p.parent = null;
+
+            // Fix replacement
+            if (p.color == BLACK)
+                fixAfterDeletion(replacement);
+        } else if (p.parent == null) { // return if we are the only node.
+            root = null;
+        } else { //  No children. Use self as phantom replacement and unlink.
+            if (p.color == BLACK)
+                fixAfterDeletion(p);
+
+            if (p.parent != null) {
+                if (p == p.parent.left)
+                    p.parent.left = null;
+                else if (p == p.parent.right)
+                    p.parent.right = null;
+                p.parent = null;
+            }
+        }
+    }
+    private void fixAfterDeletion(Entry<K,V> x) {
+        while (x != root && colorOf(x) == BLACK) {
+            if (x == leftOf(parentOf(x))) {
+                Entry<K,V> sib = rightOf(parentOf(x));
+
+                //兄为红的情况
+                //兄sib 为红色节点,则父节点和兄的孩子节点都为黑色
+                if (colorOf(sib) == RED) {
+                    //设置兄为黑色
+                    setColor(sib, BLACK);
+                    //设置父为红色
+                    setColor(parentOf(x), RED);
+                    //以父为中心，左旋转
+                    rotateLeft(parentOf(x));
+                    //此时，x的兄sib为黑色,由此转换为了兄为黑色的情况
+                    sib = rightOf(parentOf(x));
+                }
+
+                //兄为黑的情况
+                //兄的左右孩子都为黑
+                if (colorOf(leftOf(sib))  == BLACK &&
+                        colorOf(rightOf(sib)) == BLACK) {
+                    //因为x的分支由于删除，少了一个黑色节点，所以将兄分支的兄节点设置为红色，保证了各分支黑色节点的相同
+                    //设置兄为红
+                    setColor(sib, RED);
+                    //x指向父节点,在函数最后把父节点设为黑色(因为兄为红了，父就要变为黑,同时又不改变各分支黑色的数量)
+                    x = parentOf(x);
+                } else {
+                    if (colorOf(rightOf(sib)) == BLACK) {
+                        setColor(leftOf(sib), BLACK);
+                        setColor(sib, RED);
+                        rotateRight(sib);
+                        sib = rightOf(parentOf(x));
+                    }
+                    setColor(sib, colorOf(parentOf(x)));
+                    setColor(parentOf(x), BLACK);
+                    setColor(rightOf(sib), BLACK);
+                    rotateLeft(parentOf(x));
+                    x = root;
+                }
+            } else { // symmetric
+                Entry<K,V> sib = leftOf(parentOf(x));
+
+                if (colorOf(sib) == RED) {
+                    setColor(sib, BLACK);
+                    setColor(parentOf(x), RED);
+                    rotateRight(parentOf(x));
+                    sib = leftOf(parentOf(x));
+                }
+
+                if (colorOf(rightOf(sib)) == BLACK &&
+                        colorOf(leftOf(sib)) == BLACK) {
+                    setColor(sib, RED);
+                    x = parentOf(x);
+                } else {
+                    if (colorOf(leftOf(sib)) == BLACK) {
+                        setColor(rightOf(sib), BLACK);
+                        setColor(sib, RED);
+                        rotateLeft(sib);
+                        sib = leftOf(parentOf(x));
+                    }
+                    setColor(sib, colorOf(parentOf(x)));
+                    setColor(parentOf(x), BLACK);
+                    setColor(leftOf(sib), BLACK);
+                    rotateRight(parentOf(x));
+                    x = root;
+                }
+            }
+        }
+
+        setColor(x, BLACK);
+    }
+    final Entry<K,V> getEntry(Object key) {
+        // Offload comparator-based version for sake of performance
+        if (comparator != null)
+            return getEntryUsingComparator(key);
+        if (key == null)
+            throw new NullPointerException();
+        @SuppressWarnings("unchecked")
+        Comparable<? super K> k = (Comparable<? super K>) key;
+        Entry<K,V> p = root;
+        while (p != null) {
+            int cmp = k.compareTo(p.key);
+            if (cmp < 0)
+                p = p.left;
+            else if (cmp > 0)
+                p = p.right;
+            else
+                return p;
+        }
+        return null;
+    }
+    final Entry<K,V> getEntryUsingComparator(Object key) {
+        @SuppressWarnings("unchecked")
+        K k = (K) key;
+        Comparator<? super K> cpr = comparator;
+        if (cpr != null) {
+            Entry<K,V> p = root;
+            while (p != null) {
+                int cmp = cpr.compare(k, p.key);
+                if (cmp < 0)
+                    p = p.left;
+                else if (cmp > 0)
+                    p = p.right;
+                else
+                    return p;
+            }
+        }
+        return null;
+    }
     /** From CLR */
     private void rotateRight(Entry<K,V> p) {
         if (p != null) {
