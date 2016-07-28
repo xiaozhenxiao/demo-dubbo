b6abf292efb0caa73b74ca2e35821b34f81d98fb
王振 <wangzhen@hanyun.com>
2016-07-28 11:06:33 +0800

    增加springController单例测试

--- wz-java-test/src/main/java/com/wz/web/controller/VelocityController.java
+++ wz-java-test/src/main/java/com/wz/web/controller/VelocityController.java
@@ -1,5 +1,6 @@
 package com.wz.web.controller;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RequestMapping(value="/vm")
 @Controller
+@Scope("prototype")//默认controller是单例的
 public class VelocityController {
+    private int index = 0;
 
         @RequestMapping(value="/index")
         public String index(Model model) {
             String name = "tester";
             model.addAttribute("name", name);
+            model.addAttribute("index", ++index);
             return "index";
         }
 }
