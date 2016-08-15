f9dac509573dcae4299dd7354a60d3e54b2c9706
王振 <wangzhen@hanyun.com>
2016-08-15 15:06:04 +0800

    优化

--- wz-consumer/src/main/java/com/wz/jdk/java/thread/atomic/AtomicTest.java
+++ wz-consumer/src/main/java/com/wz/jdk/java/thread/atomic/AtomicTest.java
@@ -20,7 +20,7 @@ public class AtomicTest {
         // 获取Person的class对象
         Class cls = Person.class;
         // 新建AtomicLongFieldUpdater对象，传递参数是“class对象”和“long类型在类中对应的名称”
-        AtomicLongFieldUpdater mAtoLong = AtomicLongFieldUpdater.newUpdater(cls, "id");
+        AtomicLongFieldUpdater<Person> mAtoLong = AtomicLongFieldUpdater.newUpdater(cls, "id");
         Person person = new Person(12345678L);
 
         // 比较person的"id"属性，如果id的值为12345678L，则设置为1000。
