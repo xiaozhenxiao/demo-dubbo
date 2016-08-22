b1a80c9d56c1b6560276dd6c8c5cf5e19205bfad
王振 <wangzhen@hanyun.com>
2016-08-22 20:25:28 +0800

    java字节码操作javassist使用

---
+++ wz-java-test/src/main/java/com/wz/java/clazz/Javassist.java
@@ -0,0 +1,28 @@
+package com.wz.java.clazz;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
+/**
+ * TODO
+ *
+ * @author wangzhen
+ * @version 1.0
+ * @date 2016-08-22 20:15
+ **/
+public class Javassist {
+    public static void main(String[] args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        //创建Programmer类
+        CtClass cc= pool.makeClass("com.samples.Programmer");
+        //定义code方法
+        CtMethod method = CtNewMethod.make("public void code(){}", cc);
+        //插入方法代码
+        method.insertBefore("System.out.println(\"I'm a Programmer,Just Coding.....\");");
+        cc.addMethod(method);
+        //保存生成的字节码
+        cc.writeFile("d://temp");
+    }
+}
