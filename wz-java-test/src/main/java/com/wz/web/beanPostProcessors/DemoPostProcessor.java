e6c5eb3e93a1af83f38f74f623acc1251301816c
wangzhen332 <wangzhen23@jd.com>
2017-06-08 20:03:35 +0800

    优化启动

--- wz-java-test/src/main/java/com/wz/web/beanPostProcessors/DemoPostProcessor.java
+++ wz-java-test/src/main/java/com/wz/web/beanPostProcessors/DemoPostProcessor.java
@@ -1,5 +1,6 @@
 package com.wz.web.beanPostProcessors;
 
+import com.alibaba.fastjson.JSON;
 import com.wz.web.domain.Demo;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class DemoPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(beanName + "++++++++++postProcessBeforeInitialization");
         if(bean instanceof Demo){
             Demo user = (Demo)bean;
             user.setUsername("新名字");
@@ -21,6 +23,7 @@ public class DemoPostProcessor implements BeanPostProcessor {
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println(beanName + "-----postProcessAfterInitialization");
         return bean;
     }
 }
