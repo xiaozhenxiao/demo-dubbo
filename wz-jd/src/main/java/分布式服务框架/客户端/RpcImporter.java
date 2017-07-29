6496b802df4bdd9e8f02b6d1cf30fdcce5880054
wangzhen332 <wangzhen23@jd.com>
2017-07-29 19:08:52 +0800

    修改项目名称

---
+++ wz-jd/src/main/java/分布式服务框架/客户端/RpcImporter.java
@@ -0,0 +1,9 @@
+package 分布式服务框架.客户端;
+
+/**
+ * TODO
+ * wangzhen23
+ * 2017/6/13.
+ */
+public class RpcImporter {
+}
---
+++ wz-jd/src/main/java/分布式服务框架/服务端/RpcExproter.java
@@ -0,0 +1,9 @@
+package 分布式服务框架.服务端;
+
+/**
+ * TODO
+ * wangzhen23
+ * 2017/6/13.
+ */
+public class RpcExproter {
+}
