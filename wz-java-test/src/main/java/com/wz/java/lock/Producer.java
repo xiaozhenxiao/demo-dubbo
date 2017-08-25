b89b16386ed1b570615a739849ad7681dd385575
wangzhen332 <wangzhen23@jd.com>
2017-08-25 11:00:28 +0800

    java wait notify notifyAll

---
+++ wz-java-test/src/main/java/com/wz/java/lock/Producer.java
@@ -0,0 +1,46 @@
+package com.wz.java.lock;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 锁池:假设线程A已经拥有了某个对象(注意:不是类)的锁，而其它的线程想要调用这个对象的某个synchronized方法(或者synchronized块)，
+ * 由于这些线程在进入对象的synchronized方法之前必须先获得该对象的锁的拥有权，但是该对象的锁目前正被线程A拥有，所以这些线程就进入了该对象的锁池中。
+ * 等待池:假设一个线程A调用了某个对象的wait()方法，线程A就会释放该对象的锁后，进入到了该对象的等待池中。
+ * notifyAll调用后，会将全部线程由等待池移到锁池，然后参与锁的竞争，竞争成功则继续执行，如果不成功则留在锁池等待锁被释放后再次参与竞争。
+ * notify调用后，只会将等待池中的一个随机线程移到锁池。
+ * wangzhen23
+ * 2017/8/25.
+ */
+public class Producer implements Runnable {
+    List<Integer> cache;
+
+    public Producer(List<Integer> cache) {
+        this.cache = cache;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            produce();
+        }
+    }
+
+    private void produce() {
+        synchronized (cache) {
+            try {
+                while (cache.size() == 1) {
+                    cache.wait();
+                }
+
+                // 模拟一秒生产一条消息
+                Thread.sleep(1000);
+                cache.add(new Random().nextInt());
+
+                cache.notify();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ wz-java-test/src/main/java/com/wz/java/lock/WaitNotifyTest.java
@@ -0,0 +1,27 @@
+package com.wz.java.lock;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * 锁池:假设线程A已经拥有了某个对象(注意:不是类)的锁，而其它的线程想要调用这个对象的某个synchronized方法(或者synchronized块)，
+ * 由于这些线程在进入对象的synchronized方法之前必须先获得该对象的锁的拥有权，但是该对象的锁目前正被线程A拥有，所以这些线程就进入了该对象的锁池中。
+ * 等待池:假设一个线程A调用了某个对象的wait()方法，线程A就会释放该对象的锁后，进入到了该对象的等待池中。
+ * 线程调用了对象的 wait()方法，便会处于该对象的等待池中，等待池中的线程不会去竞争该对象的锁（即不会参与线程调度，大家理解就好～～）。
+ * notifyAll调用后，会将全部线程由等待池移到锁池，然后参与锁的竞争，竞争成功则继续执行，如果不成功则留在锁池等待锁被释放后再次参与竞争。
+ * notify调用后，只会将等待池中的一个随机线程移到锁池。
+ * wangzhen23
+ * 2017/8/25.
+ */
+public class WaitNotifyTest {
+    public static void main(String[] args) throws Exception {
+        List<Integer> cache = Lists.newArrayList();
+        new Thread(new Consumer(cache)).start();
+        new Thread(new Consumer(cache)).start();
+        new Thread(new Consumer(cache)).start();
+        new Thread(new Producer(cache)).start();
+        new Thread(new Producer(cache)).start();
+        new Thread(new Producer(cache)).start();
+    }
+}
