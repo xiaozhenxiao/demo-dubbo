5db7d7f5851a95322fc59517ec9e7d8c1b6ca2bb
wangzhen332 <wangzhen23@jd.com>
2017-09-27 17:55:12 +0800

    java

--- wz-java/src/test/java/JavaTest.java
+++ wz-java/src/test/java/JavaTest.java
@@ -1,3 +1,6 @@
+import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * TODO
  * wangzhen23
@@ -6,10 +9,11 @@
 public class JavaTest {
     static {
         ii = 0;
-        ii = 5+1;
+        ii = 5 + 1;
     }
 
     static int ii = 1;
+
     public static void main(String[] args) {
         int i = 0x0001;
         int j = 0x0010;
@@ -20,12 +24,15 @@ public class JavaTest {
         int o = 0x2000;
         int p = 0x4000;
 
-        int a = 11;
-        String aHex = Integer.toHexString(a);
-        System.out.println("====" + aHex);
-        System.out.printf("a=0x%04x\n", a);
-//        System.out.printf("j=0x%04x\n", j);
-//        System.out.printf("a=0x%04x\n", a);
+        int HASH_INCREMENT = 0x61c88647;
+        AtomicInteger nextHashCode = new AtomicInteger();
+        for (int q = 0; q < 16; q++) {
+            int index = nextHashCode.getAndAdd(HASH_INCREMENT) & 15;
+            System.out.println(q + "， 十六进制" + Integer.toHexString(index) + " 十进制：" + index);
+//            System.out.println(q + "， 二进制：" + Integer.toBinaryString(index));
+        }
+
+        System.out.printf("HASH_INCREMENT=0x%04x\n", HASH_INCREMENT);
 
         System.out.println("二进制 i：" + Integer.toBinaryString(i));
         System.out.println("二进制 j：" + Integer.toBinaryString(j));
@@ -35,7 +42,7 @@ public class JavaTest {
         System.out.println("二进制 n：" + Integer.toBinaryString(n));
         System.out.println("二进制 o：" + Integer.toBinaryString(o));
         System.out.println("二进制 p：" + Integer.toBinaryString(p));
-        System.out.println("二进制 a：" + Integer.toBinaryString(a));
+        System.out.println("二进制 HASH_INCREMENT：" + Integer.toBinaryString(HASH_INCREMENT));
 
         System.out.println("inc:" + inc());
 
@@ -44,19 +51,28 @@ public class JavaTest {
         String name = "12)34567890";
         int ix = name.indexOf(')');
         System.out.println("index:" + ix);
-        System.out.println("subStart:" + name.substring(0, ix+1));
-        System.out.println("substring:"+name.substring(ix+1));
+        System.out.println("subStart:" + name.substring(0, ix + 1));
+        System.out.println("substring:" + name.substring(ix + 1));
+
+        Stack<String> javaCodeStack = new Stack<String>();
+        javaCodeStack.push("var1");
+        javaCodeStack.push("var2");
+        javaCodeStack.push(javaCodeStack.pop() + "+" + javaCodeStack.pop());
+        javaCodeStack.push("return " + javaCodeStack.pop());
+        for (String s : javaCodeStack) {
+            System.out.println("=====:" + s);
+        }
     }
 
-    public static int inc(){
+    public static int inc() {
         int x;
         try {
             x = 1;
             return x;
-        }catch (Exception e){
+        } catch (Exception e) {
             x = 2;
             return x;
-        }finally {
+        } finally {
             x = 3;
         }
     }
